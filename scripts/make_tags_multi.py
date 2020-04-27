ee0ab372ab119a88d683d46ee8ade6ca120d98af
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2020-04-27 22:21:02 +0530

    add tagger script

--- scripts/make_tags_multi.py
+++ scripts/make_tags_multi.py
@@ -1,39 +1,35 @@
+import nltk
+nltk.download('stopwords')
+
 import sqlite3
 from multiprocessing import pool
 from nltk.corpus import stopwords
 from string import punctuation
 
 
-
 def execute_commands(cmds):
-    # global
-    local_list = []
 
-    conn = sqlite3.connect('../src/Data/Movie.db')
-    # create cursor to browese db
-    c = conn.cursor()
+    local_list = []
     # generating unworthy words
     unworthy_words = {x: '' for x in set(stopwords.words('english'))}
     for item in cmds:
         final_tags = []
-        title = item[0]
-        first_list = [i for i in item[0].strip(punctuation).split(' ')]
+        title = item[1]
+        first_list = [i for i in title.strip(punctuation).split(' ')]
         extracted_words = list(first_list)
         for word in extracted_words:
             try:
                 test = unworthy_words[word]
             except KeyError:
                 final_tags.append(word)
-        final_tags = str(final_tags)
+        final_tags = ','.join(final_tags)
         # print('\n\tName: {}\n\tTags: {}\n'.format(item[0], final_tags))
 
-        local_list.append((title, final_tags))
-        return local_list
+        local_list.append((item[0],title, final_tags))
+    return local_list
 
 
 def func():
-
-    global global_list
 
     conn = sqlite3.connect('../src/Data/Movie.db')
 
@@ -41,7 +37,7 @@ def func():
     c = conn.cursor()
 
     # query db
-    c.execute("SELECT title FROM Movie")
+    c.execute("SELECT id,title FROM Movie;")
 
     # function to chunk the entire data
     def chunks(l, n):
@@ -69,15 +65,18 @@ def func():
     # print(total)
 
     for each in total:
-        title, final_tags = each
-        print(f'\n Title: {title} \n Tags: {final_tags} \n')
+        pid, title, final_tags = each
+        # print(f'\n Title: {title} \n Tags: {final_tags} \n')
 
-        c.execute("""UPDATE Movie SET tag_list = :tag_list WHERE title = :title;""",
-                  {'title': str(title), 'tag_list': final_tags})
+        sql_update_query = '''Update Movie set tag_list = "{}" where id = {};'''.format(final_tags, pid)
+        # print(sql_update_query)
+        c.execute(sql_update_query)
+
+        # c.execute("""UPDATE Movie SET tag_list = :tag_list WHERE title = :title;""",
+        #           {'title': str(title), 'tag_list': final_tags})
     conn.commit()
     c.close()
 
 if __name__ == "__main__":
-    global_list = []
     func()
 
