70d844034b525a455ebcb86681acab8cf25c44b8
Tanuj Sood <tanuj.sood12@gmail.com>
2020-05-02 07:23:12 +0530

    Adding slight changes

--- src/web/views.py
+++ src/web/views.py
@@ -6,13 +6,15 @@ import sqlite3
 
 
 class Movie:
-    def __init__(self, title=None, description=None, img_url=None, genre=None, runtime=None, imdb_url=None):
+    def __init__(self, title=None, description=None, img_url=None, genre=None, runtime=None, imdb_url=None, rating=None, metascore=None):
         self.title = title if title else None
         self.img = img_url if img_url else None
         self.runtime = runtime if runtime else None
         self.genre = genre if genre else None
         self.imdb = imdb_url if imdb_url else "web/static/images/stock.jpeg"
         self.description = description if description else None
+        self.rating = rating if rating else None
+        self.metascore = metascore if metascore else None
 
 
 def getMovies(id_list=[]):
@@ -26,11 +28,11 @@ def getMovies(id_list=[]):
             _ = exists[movie_id]
         except KeyError:
             exists[movie_id] = None
-            c.execute(""" SELECT title, description, img_url, genre, runtime, imdb_url FROM Movie WHERE id = :id """, {
+            c.execute(""" SELECT title, description, img_url, genre, runtime, imdb_url, rating, metascore FROM Movie WHERE id = :id """, {
                       'id': movie_id})
             movie = c.fetchone()
             Movies.append(
-                Movie(title=movie[0], description=movie[1], img_url=movie[2], genre='/ '.join(eval(movie[3])), runtime=movie[4], imdb_url=movie[5]))
+                Movie(title=movie[0], description=movie[1], img_url=movie[2], genre='/ '.join(eval(movie[3])), runtime=movie[4], imdb_url=movie[5], rating=movie[6], metascore=movie[7]))
     conn.close()
     return Movies
 
